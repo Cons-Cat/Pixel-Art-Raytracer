31733aa4bfce299c730c4a334339285b62a10ee4
Conscat <wgooch2000@gmail.com>
2021-11-12 17:23:41 -0500

    Made run path for Windows

--- main.cpp
+++ main.cpp
@@ -6,18 +6,28 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <string>
 
-#include "vulkan/vulkan_core.h"
-
-// Linux-specific executable path.
+#ifdef WIN32
+#include <windows.h>
+inline auto get_run_path() -> std::string {
+    std::array<char, MAX_PATH> result{};
+    std::string full_path = std::string(
+        result.data(), GetModuleFileName(NULL, result.data(), MAX_PATH));
+    return std::string(full_path.substr(0, full_path.find_last_of('\\\\'))) +
+           "/";
+}
+#else
+#include <linux/limits.h>
 #include <unistd.h>
-auto get_run_path() -> std::string {
+inline auto get_run_path() -> std::string {
     std::array<char, PATH_MAX - NAME_MAX> result{};
     ssize_t count =
         readlink("/proc/self/exe", result.data(), PATH_MAX - NAME_MAX);
     std::string full_path = std::string(result.data());
     return std::string(full_path.substr(0, full_path.find_last_of('/'))) + "/";
 }
+#endif
 
 constexpr uint32_t view_width = 480;
 constexpr uint32_t view_height = 300;
