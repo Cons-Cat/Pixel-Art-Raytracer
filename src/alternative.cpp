0f8f2ad5a9f5e972caf974e327be53e4a075e413
Conscat <wgooch2000@gmail.com>
2022-01-11 21:21:23 -0500

    Major refactor

--- src/alternative.cpp
+++ src/alternative.cpp
@@ -75,7 +75,7 @@ struct Entities {
     };
 
     // TODO: Consider perfect forwarding or move:
-    void insert(Entity entity) {
+    void insert(Entity const entity) {
         aabbs[last_entity_index] = entity.aabb;
         positions[last_entity_index] = entity.position;
         colors[last_entity_index] = entity.color;
@@ -87,70 +87,35 @@ struct Entities {
     }
 };
 
-template <typename T>
-struct OneDArr {
-    std::vector<T> data;
-    OneDArr() = delete;
-    OneDArr(std::integral auto const width) {
-        data.reserve(width);
-    }
-    auto operator[](std::integral auto x) -> T& {
-        return data[x];
-    }
-};
-
-template <typename T>
-struct TwoDArr {
-    std::vector<OneDArr<T>> data;
-    TwoDArr() = delete;
-    TwoDArr(std::integral auto const width, std::integral auto const height) {
-        data.reserve(height);
-        for (auto& d : data) {
-            d(width);
-        }
-    }
-    auto operator[](std::integral auto y) -> OneDArr<T>& {
-        return data[y];
-    }
-};
-
-template <typename T>
-struct ThreeDArr {
-    std::vector<TwoDArr<T>> data;
-    ThreeDArr() = delete;
-    ThreeDArr(std::integral auto const length, std::integral auto const width,
-              std::integral auto const height) {
-        data.reserve(length);
-        for (auto& d : data) {
-            d(height);
-        }
-    }
-    auto operator[](std::integral auto z) -> TwoDArr<T>& {
-        return data[z];
-    }
-};
-
 constexpr int16_t cell_size = 20;
 constexpr int32_t view_width = 480;
 constexpr int32_t view_height = 320;
-constexpr int32_t cells_count_in_view_width = view_width / cell_size;
-constexpr int32_t cells_count_in_view_height = view_height / cell_size;
-constexpr int32_t cells_count_in_view_length = view_height / cell_size;
+constexpr int32_t bin_count_in_view_width = view_width / cell_size;
+constexpr int32_t bin_count_in_view_height = view_height * 2 / cell_size;
+constexpr int32_t bin_count_in_view_length = view_height / cell_size;
+
+auto index_view_cube(int x, int y, int z) -> int {
+    return (x * bin_count_in_view_height * bin_count_in_view_length) +
+           // The y must be offset by the number of cells below the view's
+           // origin, to prevent negative indexing.
+           ((y + bin_count_in_view_height / 2) * bin_count_in_view_length) + z;
+}
 
 auto main() -> int {
     constexpr int entity_count = 20;
-    Entities<entity_count> entities;
+    auto p_entities = new (std::nothrow) Entities<entity_count>;
 
-    // The AABB volume is currently hard-coded to 20^3.
-    for (int i = 0; i < entities.size(); i++) {
-        int32_t x = rand() % (view_width - 20);
-        int32_t y = rand() % (view_height - 40);
-        int32_t z =
-            rand() % (view_height - 40 - y);  // This may be slightly too large.
+    // An AABB's volume is currently hard-coded to 20^3.
+    for (int i = 0; i < p_entities->size(); i++) {
+        // Place entities randomly throughout a cube which bounds the
+        // orthographic view frustrum, assuming the camera is at <0,0,0>.
+        int32_t x = rand() % (view_width);
+        int32_t y = (rand() % (view_height * 2)) - view_height;
+        int32_t z = rand() % (view_height);
 
         Point<int32_t> new_position = {x, y, z};
 
-        entities.insert({
+        p_entities->insert({
             .aabb = {.min_point = {static_cast<int16_t>(new_position.x),
                                    static_cast<int16_t>(new_position.y),
                                    static_cast<int16_t>(new_position.z)},
@@ -162,103 +127,59 @@ auto main() -> int {
         });
     }
 
-    int32_t aabb_to_entity_index_map[cells_count_in_view_width]
-                                    [cells_count_in_view_height]
-                                    [cells_count_in_view_length];
+    // p_entities is random-access, but the bins they're stored into are not, so
+    // we must store a random-access map to the entities' attributes.
+    auto p_aabb_index_to_entity_index_map = new (std::nothrow)
+        int32_t[bin_count_in_view_width * bin_count_in_view_height *
+                bin_count_in_view_length];
 
-    // Determine how many entities fit into each entity bin.
-    int8_t aabb_count_in_bin[cells_count_in_view_width]
-                            [cells_count_in_view_height]
-                            [cells_count_in_view_length];
-    int aabbs_count_in_view = 0;
+    // Track how many entities fit into each bin.
+    auto p_aabb_count_in_bin = new (
+        std::nothrow) int[bin_count_in_view_width * bin_count_in_view_height *
+                          bin_count_in_view_length];
+    int aabbs_count_in_view_cube = 0;
 
-    for (int i = 0; i < entities.size(); i++) {
-        AABB& this_aabb = entities.aabbs[i];
+    auto* p_aabb_bins = new (std::nothrow) AABB[aabbs_count_in_view_cube];
+    int entity_count_currently_in_bin[bin_count_in_view_width *
+                                      bin_count_in_view_height *
+                                      bin_count_in_view_length];
 
-        // TODO: If this entity is inside the view bounds.
-        // if ((this_aabb.min_point.x >= 0) && (this_aabb.max_point.x <
-        // view_width)
-        //     && (this_aabb.min_point.y >= 0) &&
-        //     (this_aabb.max_point.y < view_height / 2) &&
-        //     (this_aabb.min_point.z >= 0) &&
-        //     (this_aabb.max_point.z < view_height / 2)
-        // ) {
+    // Determine the address offset of each entity AABB bin.
+    auto* p_aabb_bin_index_offset = new (
+        std::nothrow) int[bin_count_in_view_width * bin_count_in_view_height *
+                          bin_count_in_view_length];
+
+    for (int i = 0; i < p_entities->size(); i++) {
+        AABB& this_aabb = p_entities->aabbs[i];
 
         // Get the cells that this AABB fits into.
         int min_x_index = this_aabb.min_point.x / cell_size;
         int min_y_index = this_aabb.min_point.y / cell_size;
         int min_z_index = this_aabb.min_point.z / cell_size;
+
         int max_x_index = this_aabb.max_point.x / cell_size;
         int max_y_index = this_aabb.max_point.y / cell_size;
         int max_z_index = this_aabb.max_point.z / cell_size;
+
+        // TODO: Test if this entity is inside the view frustrum.
 
         // Place this AABB into every bin that it spans across.
         for (int x = min_x_index; x <= max_x_index; x++) {
-            for (int y = min_x_index; y <= max_y_index; y++) {
-                for (int z = min_x_index; z <= max_z_index; z++) {
-                    aabb_count_in_bin[x][y][z] += 1;
-                    aabbs_count_in_view += 1;
-                    aabb_to_entity_index_map[x][y][z] = i;
+            for (int y = min_y_index; y <= max_y_index; y++) {
+                for (int z = min_z_index; z <= max_z_index; z++) {
+                    if (x < bin_count_in_view_width &&
+                        y < bin_count_in_view_height &&
+                        z < bin_count_in_view_length) {
+                        p_aabb_bins[index_view_cube(x, y, z)] = this_aabb;
+
+                        aabbs_count_in_view_cube += 1;
+                        p_aabb_count_in_bin[index_view_cube(x, y, z)] += 1;
+                        p_aabb_index_to_entity_index_map[index_view_cube(
+                            x, y, z)] = i;
+                    }
                 }
             }
         }
-        // }
-    }
-
-    // Determine the address offset of each entity bin.
-    int aabb_bin_index_offset[cells_count_in_view_width]
-                             [cells_count_in_view_height]
-                             [cells_count_in_view_length];
-    int aabb_index_offset_accumulator = 0;
-    for (int x = 0; x < cells_count_in_view_width; x++) {
-        for (int y = 0; y < cells_count_in_view_height; y++) {
-            for (int z = 0; z < cells_count_in_view_length; z++) {
-                aabb_bin_index_offset[x][y][z] = aabb_index_offset_accumulator *
-                                                 static_cast<int>(sizeof(AABB));
-                aabb_index_offset_accumulator += aabb_count_in_bin[x][y][z];
-            }
-        }
-    }
-
-    AABB* p_aabb_bins = new (std::nothrow) AABB[aabbs_count_in_view];
-    int8_t entity_count_currently_in_bin[cells_count_in_view_width]
-                                        [cells_count_in_view_height]
-                                        [cells_count_in_view_length];
-
-    for (int i = 0; i < aabbs_count_in_view; i++) {
-        AABB& this_aabb = entities.aabbs[i];
-        // TODO: Consider entity's dimensions.
-        // if ((this_aabb.min_point.x >= 0) &&
-        //     (this_aabb.max_point.x < view_width) &&
-        //     (this_aabb.min_point.y >= 0) &&
-        //     (this_aabb.max_point.y < view_height / 2) &&
-        //     (this_aabb.min_point.z >= 0) &&
-        //     (this_aabb.max_point.z < view_height / 2)) {
-
-        // Get the cells that this AABB fits into.
-        int min_x_index = this_aabb.min_point.x / cell_size;
-        int min_y_index = this_aabb.min_point.y / cell_size;
-        int min_z_index = this_aabb.min_point.z / cell_size;
-        int max_x_index = this_aabb.max_point.x / cell_size;
-        int max_y_index = this_aabb.max_point.y / cell_size;
-        int max_z_index = this_aabb.max_point.z / cell_size;
-
-        // Place this AABB into every bin that it spans across.
-        for (int x = min_x_index; x < max_x_index; x++) {
-            for (int y = min_x_index; y < max_y_index; y++) {
-                for (int z = min_x_index; z < max_z_index; z++) {
-                    // Place this this entity into the bins at an address
-                    // relative to the offset of this <x,y,z> pair, plus an
-                    // offset which approaches the count of AABBs in that
-                    // bin.
-                    p_aabb_bins[aabb_bin_index_offset[x][y][z] +
-                                entity_count_currently_in_bin[x][y][z]] =
-                        this_aabb;
-                    entity_count_currently_in_bin[x][y][z] += 1;
-                }
-            }
-        }
-        // }
     }
 
     Point<int16_t> ray_direction = {
@@ -267,8 +188,7 @@ auto main() -> int {
         .z = 1,
     };
 
-    // Pixel texture[view_width][view_height];
-    auto p_texture = new Resvec<Resvec<Pixel>>(view_width, view_height);
+    auto p_texture = new (std::nothrow) Pixel[view_height * view_width];
     for (int16_t i = 0; i < view_width; i++) {
         // `j` is a ray's world-position skywards.
         for (int16_t j = 0; j < view_height * 2; j++) {
@@ -279,34 +199,42 @@ auto main() -> int {
                         .y = j,
                         .z = 0,
                     },
-                .direction = ray_direction,
                 .direction_inverse =
                     {
                         // TODO: This might not work ..
                         .x = 0,
-                        .y = static_cast<int16_t>(1 / ray_direction.y),
-                        .z = static_cast<int16_t>(1 / ray_direction.z),
+                        .y = 1,  // -1 / 1
+                        .z = 1,  //  1 / 1
                     },
             };
 
-            // `k` is a ray's world-position casting earthwards.
-            // It is 2 bytes because it operates with AABB coordinates, which
+            // `k` is a ray's world-position casting forwards.
+            // It is 2 bytes because it operates on AABB coordinates, which
             // are 2 bytes.
-            for (int16_t k = 0; j - k >= 0; k++) {
-                if (j - k < view_height) {
+            for (int16_t k = 0; k < bin_count_in_view_length; k++) {
+                int16_t bin_x = static_cast<int16_t>(i / cell_size);
+                int16_t bin_y = static_cast<int16_t>((j - k) / cell_size);
+                int16_t bin_z = static_cast<int16_t>(k / cell_size);
+
+                if (bin_y < bin_count_in_view_height) {
                     continue;
                 }
-                int16_t cell_x = static_cast<int16_t>(i / cell_size);
-                int16_t cell_y = static_cast<int16_t>((j - k) / cell_size);
-                int16_t cell_z = static_cast<int16_t>(k / cell_size);
 
-                Pixel color = {0, 0, 0};
+                Pixel background_color = {0, 0, 0};
                 int16_t closest_entity_depth =
                     std::numeric_limits<int16_t>::max();
-                for (int ii = 0; ii < aabb_count_in_bin[cell_x][cell_y][cell_z];
-                     ii++) {
-                    AABB& this_aabb = p_aabb_bins
-                        [aabb_bin_index_offset[cell_x][cell_y][cell_z] + ii];
+
+                int entities_in_this_bin =
+                    p_aabb_count_in_bin[index_view_cube(bin_x, bin_y, bin_z)];
+
+                for (int this_bin_entity_index = 0;
+                     this_bin_entity_index <
+                     p_aabb_count_in_bin[index_view_cube(bin_x, bin_y, bin_z)];
+                     this_bin_entity_index++) {
+                    AABB& this_aabb =
+                        p_aabb_bins[p_aabb_bin_index_offset[index_view_cube(
+                                        bin_x, bin_y, bin_z)] +
+                                    this_bin_entity_index];
 
                     // TODO: If this entity has closer depth.
                     // if (this_aabb.min_point.y + (j - this_aabb.min_point.y) >
@@ -314,17 +242,17 @@ auto main() -> int {
 
                     // Intersect ray with this aabb.
                     if (this_aabb.intersect(this_ray)) {
-                        color =
-                            entities
-                                .colors[aabb_to_entity_index_map[cell_x][cell_y]
-                                                                [cell_z]];
+                        background_color =
+                            p_entities->colors[p_aabb_index_to_entity_index_map
+                                                   [index_view_cube(
+                                                       bin_x, bin_y, bin_z)]];
                         // TODO: Update `closest_entity_depth`.
                         // TODO: Break ray when a cell has any hits.
                     }
                     // }
                 }
 
-                p_texture[i][j] = color;
+                p_texture[(j - k) * view_width + i] = background_color;
             }
         }
     }
