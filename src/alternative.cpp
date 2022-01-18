e3942b5395cba21dff0c46ba6818b91a78f598d9
Conscat <wgooch2000@gmail.com>
2022-01-17 20:04:30 -0500

    Refactors

--- src/alternative.cpp
+++ src/alternative.cpp
@@ -1,6 +1,8 @@
 #include <liblava/lava.hpp>
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_surface.h>
@@ -24,8 +26,9 @@ struct Ray {
 static_assert(sizeof(Ray) == 12);
 
 struct alignas(16) AABB {
-    Point<short> min_point;
-    Point<short> max_point;
+    // TODO: Factor into min_bound and max_bound, and update velocity with SIMD.
+    Point<short> position;
+    Point<short> extent;
 
     auto intersect(Ray& ray) -> bool {
         // Adapted from Fast, Branchless Ray/Bounding Box Intersections:
@@ -36,9 +39,9 @@ struct alignas(16) AABB {
         // X plane comparisons.
         if (ray.direction_inverse.x != 0) {
             short intersect_x_1 =
-                static_cast<short>((min_point.x - ray.origin.x));
+                static_cast<short>((position.x - ray.origin.x));
             short intersect_x_2 =
-                static_cast<short>((max_point.x - ray.origin.x));
+                static_cast<short>((position.x + extent.x - ray.origin.x));
             min_distance = std::min(intersect_x_1, intersect_x_2);
             max_distance = std::max(intersect_x_1, intersect_x_2);
         }
@@ -46,9 +49,10 @@ struct alignas(16) AABB {
         // Y plane comparisons.
         if (ray.direction_inverse.y != 0) {
             short intersect_y_1 = static_cast<short>(
-                (min_point.y - ray.origin.y) * ray.direction_inverse.y);
-            short intersect_y_2 = static_cast<short>(
-                (max_point.y - ray.origin.y) * ray.direction_inverse.y);
+                (position.y - ray.origin.y) * ray.direction_inverse.y);
+            short intersect_y_2 =
+                static_cast<short>((position.y + extent.y - ray.origin.y) *
+                                   ray.direction_inverse.y);
             min_distance =
                 std::max(min_distance, std::min(intersect_y_1, intersect_y_2));
             max_distance =
@@ -58,9 +62,10 @@ struct alignas(16) AABB {
         // Z plane comparisons.
         if (ray.direction_inverse.z != 0) {
             short intersect_z_1 = static_cast<short>(
-                (min_point.z - ray.origin.z) * ray.direction_inverse.z);
-            short intersect_z_2 = static_cast<short>(
-                (max_point.z - ray.origin.z) * ray.direction_inverse.z);
+                (position.z - ray.origin.z) * ray.direction_inverse.z);
+            short intersect_z_2 =
+                static_cast<short>((position.z + extent.z - ray.origin.z) *
+                                   ray.direction_inverse.z);
             min_distance =
                 std::max(min_distance, std::min(intersect_z_1, intersect_z_2));
             max_distance =
@@ -81,20 +86,17 @@ struct Pixel {
 template <int entity_count>
 struct Entities {
     AABB aabbs[entity_count];
-    Point<int> positions[entity_count];
     Pixel colors[entity_count];  // TODO: Make sprite.
     int last_entity_index = 0;
 
     using Entity = struct {
         AABB aabb;
-        Point<int> position;
         Pixel color;
     };
 
     // TODO: Consider perfect forwarding or move:
     void insert(Entity const entity) {
         aabbs[last_entity_index] = entity.aabb;
-        positions[last_entity_index] = entity.position;
         colors[last_entity_index] = entity.color;
         last_entity_index += 1;
     }
@@ -128,30 +130,33 @@ void count_entities_in_bins(Entities<entity_count>* p_entities,
     for (int i = 0; i < p_entities->size(); i++) {
         AABB& this_aabb = p_entities->aabbs[i];
 
+        int min_x_world = this_aabb.position.x;
+        int min_y_world = this_aabb.position.y;
+        int min_z_world = this_aabb.position.z;
+        int max_x_world = min_x_world + this_aabb.extent.x;
+        int max_y_world = min_y_world + this_aabb.extent.y;
+        int max_z_world = min_z_world + this_aabb.extent.z;
+
         // Skip this entity if it is outside the view bounds.
-        if ((this_aabb.max_point.x <= 0) ||
-            (this_aabb.min_point.x > view_width) ||
-            (this_aabb.max_point.y <= -view_height) ||
-            (this_aabb.min_point.y > view_height) ||
-            (this_aabb.max_point.z <= 0) ||
-            (this_aabb.min_point.z > view_height)) {
+        if ((max_x_world < 0) || (min_x_world > view_width) ||
+            (max_y_world < -view_height) || (min_y_world > view_height) ||
+            (max_z_world < 0) || (min_z_world > view_height)) {
             break;
         }
 
-        // Get the cells that this AABB fits into.
-        int min_x_index = std::max(0, this_aabb.min_point.x / single_bin_size);
-        int min_y_index = std::max(
-            0, (this_aabb.min_point.y + view_height) / single_bin_size);
-        int min_z_index = std::max(0, this_aabb.min_point.z / single_bin_size);
+        // Get the cells that this `AABB` fits into.
+        int min_x_index = std::max(0, min_x_world / single_bin_size);
+        int min_y_index =
+            std::max(0, (min_y_world + view_height) / single_bin_size);
+        int min_z_index = std::max(0, min_z_world / single_bin_size);
 
         // TODO: Figure out why `+ 2` is necessary.
         int max_x_index =
-            std::min(hash_width, this_aabb.max_point.x / single_bin_size + 2);
+            std::min(hash_width, max_x_world / single_bin_size + 2);
         int max_y_index = std::min(
-            hash_height,
-            (this_aabb.max_point.y + view_height) / single_bin_size + 2);
+            hash_height, (max_y_world + view_height) / single_bin_size + 2);
         int max_z_index =
-            std::min(hash_length, this_aabb.max_point.z / single_bin_size + 2);
+            std::min(hash_length, max_z_world / single_bin_size + 2);
 
         // Place this AABB into every bin that it spans across.
         for (int bin_x = min_x_index; bin_x <= max_x_index; bin_x += 1) {
@@ -173,12 +178,6 @@ void count_entities_in_bins(Entities<entity_count>* p_entities,
 void trace_hash(Entities<entity_count>* p_entities, AABB* p_aabb_bins,
                 int* p_aabb_count_in_bin, int* p_aabb_index_to_entity_index_map,
                 Pixel* p_texture) {
-    Point<short> ray_direction = {
-        .x = 0,
-        .y = -1,
-        .z = 1,
-    };
-
     // `i` is a ray's world-position lateral to the ground, iterating
     // rightwards.
     for (short i = 0; i < view_width; i++) {
@@ -232,8 +231,8 @@ void trace_hash(Entities<entity_count>* p_entities, AABB* p_aabb_bins,
                     // }
 
                     // Intersect ray with this aabb.
-                    if (this_aabb.min_point.x <= i &&
-                        this_aabb.max_point.x >= i) {
+                    if (this_aabb.position.x <= i &&
+                        this_aabb.position.x + this_aabb.extent.x >= i) {
                         if (this_aabb.intersect(this_ray)) {
                             this_color =
                                 p_entities
@@ -303,17 +302,15 @@ auto main() -> int {
         int y = (rand() % (view_height * 2)) - view_height;
         int z = (rand() % (view_height));
 
-        Point<int> new_position = {x, y, z};
+        Point<short> new_position = {static_cast<short>(x),
+                                     static_cast<short>(y),
+                                     static_cast<short>(z)};
 
         // An AABB's volume is currently hard-coded to 10^3.
         p_entities->insert({
-            .aabb = {.min_point = {static_cast<short>(new_position.x),
-                                   static_cast<short>(new_position.y),
-                                   static_cast<short>(new_position.z)},
-                     .max_point = {static_cast<short>(new_position.x + 20),
-                                   static_cast<short>(new_position.y + 20),
-                                   static_cast<short>(new_position.z + 20)}},
-            .position = new_position,
+            .aabb = {.position = {new_position.x, new_position.y,
+                                  new_position.z},
+                     .extent = {20, 20, 20}},
 
             // Randomize colors:
             .color = {static_cast<unsigned char>(rand()),
@@ -333,6 +330,9 @@ auto main() -> int {
         });
     }
 
+    // Place player character near the center.
+    // p_entities->positions[0] = {view_width / 2, 10, view_height / 2};
+
     // TODO: Make a trivial pass-through graphics shader pipeline in Vulkan
     // to render texture.
 
@@ -348,26 +348,54 @@ auto main() -> int {
         SDL_CreateTexture(p_renderer, SDL_PIXELFORMAT_RGB888,
                           SDL_TEXTUREACCESS_STREAMING, view_width, view_height);
 
-    void* p_blit = new (std::nothrow) Pixel[view_width * view_height];
+    Pixel* p_blit = new (std::nothrow) Pixel[view_width * view_height];
+    void** p_blit_address = static_cast<void**>(static_cast<void*>(&p_blit));
 
     while (true) {
-        SDL_Event e;
-        if (SDL_PollEvent(&e)) {
-            if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE) {
-                break;
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_KEYUP:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            goto exit_loop;
+                            break;
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_LEFT:
+                            // p_entities->positions[0].x -= 10;
+                            break;
+                        case SDLK_RIGHT:
+                            // p_entities->positions[0].x += 10;
+                            break;
+                        case SDLK_UP:
+                            // p_entities->positions[0].z += 10;
+                            break;
+                        case SDLK_DOWN:
+                            // p_entities->positions[0].z -= 10;
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
             }
         }
+        // p_entities->positions[0].x -= 1;
 
+        memset(p_aabb_count_in_bin, 0, hash_cube_volume * 4);
         count_entities_in_bins(p_entities, p_aabb_bins, p_aabb_count_in_bin,
                                p_aabb_index_to_entity_index_map);
         trace_hash(p_entities, p_aabb_bins, p_aabb_count_in_bin,
                    p_aabb_index_to_entity_index_map, p_texture);
 
         int texture_pitch;
-        SDL_LockTexture(p_sdl_texture, nullptr, &p_blit, &texture_pitch);
+        SDL_LockTexture(p_sdl_texture, nullptr, p_blit_address, &texture_pitch);
         for (int row = 0; row < view_height; row++) {
-            memset(static_cast<char*>(p_blit) + row * texture_pitch, 122, 1920);
-            memcpy(static_cast<char*>(p_blit) + row * texture_pitch,
+            memset(static_cast<char*>((void*)p_blit) + row * texture_pitch, 122,
+                   1920);
+            memcpy(static_cast<char*>((void*)p_blit) + row * texture_pitch,
                    p_texture + row * view_width, view_width * sizeof(Pixel));
         }
         SDL_UnlockTexture(p_sdl_texture);
@@ -376,17 +404,24 @@ auto main() -> int {
         SDL_Rect blit_rect = {
             0, 0, static_cast<int>(texture_pitch / sizeof(Pixel)), view_height};
 
-        SDL_RenderClear(p_renderer);
+        // SDL_RenderClear(p_renderer);
         SDL_RenderCopy(p_renderer, p_sdl_texture, &view_rect, &blit_rect);
         SDL_RenderPresent(p_renderer);
     }
 
+exit_loop:
     SDL_DestroyTexture(p_sdl_texture);
     SDL_DestroyWindow(p_window);
     SDL_DestroyRenderer(p_renderer);
-    SDL_Quit();
+    SDL_VideoQuit();
 
     delete[] p_aabb_bins;
+    // Segfaults:
+    // delete[] p_aabb_count_in_bin;
+    delete p_entities;
+    delete[] p_aabb_index_to_entity_index_map;
+    // Segfaults:
+    // delete[] p_blit;
 }
 
 /* Create an entity.
