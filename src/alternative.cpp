d6f09f4a2bfff3ddf2ae16ccb5de2058565f0029
Conscat <wgooch2000@gmail.com>
2022-03-05 06:59:33 -0500

    Kind of more correct algorithm (much slower).

--- src/alternative.cpp
+++ src/alternative.cpp
@@ -353,12 +353,14 @@ void trace_hash_for_pixel(Entities<entity_count>* p_entities, AABB* p_aabb_bins,
                             color_palette[this_sprite
                                               .color[this_sprite_px_index]];
 
-                        this_color.y =
-                            this_aabb.position.y + this_aabb.extent.y +
-                            this_aabb.extent.z - sprite_px_row -
-                            this_sprite.depth[this_sprite_px_index] + 0;
+                        this_color.y = this_aabb.position.y +
+                                       this_aabb.extent.y + this_aabb.extent.z -
+                                       sprite_px_row -
+                                       this_sprite.depth[this_sprite_px_index];
                         this_color.z = this_aabb.position.z +
                                        this_sprite.depth[this_sprite_px_index];
+
+                        this_color.entity_index = this_entity_index;
 
                         has_intersected = true;
                     }
@@ -395,10 +397,11 @@ void trace_hash_for_pixel(Entities<entity_count>* p_entities, AABB* p_aabb_bins,
 }
 
 auto trace_hash_for_light(int* p_aabb_count_in_bin, AABB* p_aabb_bins,
+                          int* p_aabb_index_to_entity_index_map,
                           int const bin_x_start, int const bin_y_start,
                           int const bin_z_start, int const bin_x_end,
-                          int const bin_y_end, int const bin_z_end, Ray& ray)
-    -> bool {
+                          int const bin_y_end, int const bin_z_end,
+                          int const start_entity_index, Ray& ray) -> bool {
     // TODO: Benchmark against integer solution.
     Point<float> bin_start = {static_cast<float>(bin_x_start),
                               static_cast<float>(bin_y_start),
@@ -408,7 +411,6 @@ auto trace_hash_for_light(int* p_aabb_count_in_bin, AABB* p_aabb_bins,
                             static_cast<float>(bin_y_end),
                             static_cast<float>(bin_z_end)};
 
-    Point<float> current_bin_float = bin_start;
     Point<float> bin_distance = {bin_end.x - bin_start.x,
                                  bin_end.y - bin_start.y,
                                  bin_end.z - bin_start.z};
@@ -422,17 +424,53 @@ auto trace_hash_for_light(int* p_aabb_count_in_bin, AABB* p_aabb_bins,
                                   bin_distance.y / largest_bin_distance,
                                   bin_distance.z / largest_bin_distance};
 
+    Point<float> current_bin_float = bin_start;
+    Point<float> temp_bin_float = bin_start;
     int counter = 0;
     int start = index_into_view_hash(bin_x_start, bin_y_start, bin_z_start);
 
-    for (int i = 0; i < static_cast<int>(largest_bin_distance); i++) {
-        current_bin_float = {current_bin_float.x + bin_step_size.x,
-                             current_bin_float.y + bin_step_size.y,
-                             current_bin_float.z + bin_step_size.z};
+    for (int i = 0; i < static_cast<int>(largest_bin_distance);) {
+        // current_bin_float = {current_bin_float.x + bin_step_size.x,
+        //                      current_bin_float.y + bin_step_size.y,
+        //                      current_bin_float.z + bin_step_size.z};
+        current_bin_float = temp_bin_float;
+
+        if (counter == 0) {
+            current_bin_float.x = temp_bin_float.x + bin_step_size.x;
+            counter++;
+        } else if (counter == 1) {
+            current_bin_float.y = temp_bin_float.y + bin_step_size.y;
+            counter++;
+        } else if (counter == 2) {
+            current_bin_float.z = temp_bin_float.z + bin_step_size.z;
+            counter++;
+        } else if (counter == 3) {
+            current_bin_float.x = temp_bin_float.x + bin_step_size.x;
+            current_bin_float.y = temp_bin_float.y + bin_step_size.y;
+            counter++;
+        } else if (counter == 4) {
+            current_bin_float.x = temp_bin_float.x + bin_step_size.x;
+            current_bin_float.z = temp_bin_float.z + bin_step_size.z;
+            counter++;
+        } else if (counter == 5) {
+            current_bin_float.y = temp_bin_float.y + bin_step_size.y;
+            current_bin_float.z = temp_bin_float.z + bin_step_size.z;
+            counter++;
+        } else {
+            current_bin_float.x = current_bin_float.x + bin_step_size.x;
+            current_bin_float.y = current_bin_float.y + bin_step_size.y;
+            current_bin_float.z = current_bin_float.z + bin_step_size.z;
+            temp_bin_float = current_bin_float;
+            counter = 0;
+            i++;
+        }
 
         Point<int> current_bin = static_cast<Point<int>>(current_bin_float);
         int hash_bin_index =
             index_into_view_hash(current_bin.x, current_bin.y, current_bin.z);
+        if (start == hash_bin_index) {
+            continue;
+        }
 
         // Terminate this ray if it is obstructed in this bin.
         if (p_aabb_count_in_bin[hash_bin_index] > 0) {
@@ -441,6 +479,13 @@ auto trace_hash_for_light(int* p_aabb_count_in_bin, AABB* p_aabb_bins,
             // get sorted in superfluous bins.
             for (int j = 0; j < p_aabb_count_in_bin[hash_bin_index]; j++) {
                 int this_entity_index = hash_bin_index * sparse_bin_size + j;
+
+                // Prevent self-intersection.
+                if (start_entity_index ==
+                    p_aabb_index_to_entity_index_map[this_entity_index]) {
+                    continue;
+                }
+
                 if (p_aabb_bins[this_entity_index].intersect(ray)) {
                     return false;
                 }
@@ -677,9 +722,10 @@ auto main() -> int {
 
             // Leave the color as ambience if the light is obstructed.
             if (trace_hash_for_light(p_aabb_count_in_bin, p_aabb_bins,
+                                     p_aabb_index_to_entity_index_map,
                                      ray_bin_x, ray_bin_y, ray_bin_z,
                                      light_bin_x, light_bin_y, light_bin_z,
-                                     this_ray)) {
+                                     this_pixel.entity_index, this_ray)) {
                 // Get the dot product between this pixel's normal and
                 // the light ray's incident vector.
                 float diffuse = std::max<float>(
